6f2012575c484f64eda96b56c360cb2f8177e9c5
Chintan R Dave <chintan@zealousweb.com>
2013-04-29 12:54:00 +0530

    remove Tableview add date picker and vc file

---
+++ PopupDemo/crdave1988ViewController.m
@@ -0,0 +1,38 @@
+//
+//  crdave1988ViewController.m
+//  PopupDemo
+//
+//  Created by chintan on 29/04/13.
+//  Copyright (c) 2013 ZWT. All rights reserved.
+//
+
+#import "crdave1988ViewController.h"
+
+@interface crdave1988ViewController ()
+
+@end
+
+@implementation crdave1988ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
