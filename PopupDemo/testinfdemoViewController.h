a77efb5d5ebe8a089a47cbfd8038a13d72edd4a3
z0414 <testing.demo@gmail.com>
2013-04-29 12:52:08 +0530

    Add segue handle click

---
+++ PopupDemo/testinfdemoViewController.h
@@ -0,0 +1,15 @@
+//
+//  testinfdemoViewController.h
+//  PopupDemo
+//
+//  Created by z0414 on 29/04/13.
+//  Copyright (c) 2013 ZWT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testinfdemoViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)buttonclick:(id)sender;
+@end
